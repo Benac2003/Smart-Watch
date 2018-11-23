e6fe62fe1436af4d11ca02ada66564f18ed34a1b
Benjamynn Weber <Benac2003@B23s-MacBook-Air.local>
2018-11-23 08:39:13 +0800

    update

--- displaytest.py
+++ displaytest.py
@@ -8,24 +8,17 @@ from kivy.lang import *
 from StoringVariables import *
 
 #update time and date
-def Time(ticks, hour, second, minute,day, month, year):
-    time = "%02d : %02d : %02d" % (hour, minute, second)
-    date = "%02d/%02d/%04d" % (day, month, year)
-    return time, date
 
 #Imports data from app.kv
 class Lockscreen(Widget):
-    time = StringProperty()
     def Updatetime(self, **kwargs):
         super(Lockscreen, self).Updatetime(**kwargs)
     def change_text(self):
         #update time
-        ticks, hour, minute, second, day, month, year, pm = Data_get()
-        self.time = self.time = "%02d : %02d : %02d" % (hour, minute, second)
+  
 
 #builds gui
 class app(App):
-
     def build(self):
         return Lockscreen()
 app().run()
