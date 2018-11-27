3f528400fab4277fe9bf8f0705ad52fe7199799c
Benjamynn Weber <Benac2003@B23s-MacBook-Air.local>
2018-11-27 12:20:43 +0800

    update

--- displaytest.py
+++ displaytest.py
@@ -1,25 +1,14 @@
-
-#kivy
+import kivy
+kivy.require("1.9.0")
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
-from kivy.lang import *
-from StoringVariables import *
-
-#update time and date
-
-#Imports data from app.kv
 class Lockscreen(Widget):
-    def Updatetime(self, **kwargs):
-        super(Lockscreen, self).Updatetime(**kwargs)
-    def change_text(self):
-        #update time
-  
+    pass
 
-#builds gui
-class app(App):
+class Main_App(App):
+    
     def build(self):
         return Lockscreen()
-app().run()
 
+Smart-Watch = Main_App()
+Smart-Watch.run()
