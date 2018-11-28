38ecbffd1b8cc6751be3569505ad120cbcc1f482
Benjamynn Weber <Benac2003@B23s-Air.lan>
2018-11-28 21:04:26 +0800

    update

--- displaytest.py
+++ displaytest.py
@@ -5,9 +5,20 @@ from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
 from kivy.core.window import Window
-
+from kivy.uix.screenmanager import Screen, ScreenManager
 class CustomPopup(Popup):
     pass
+
+class Homescreen(Screen):
+    pass
+
+class Lockscreen(Screen):
+    pass
+
+screen_manager = ScreenManager()
+
+screen_manager.add_widget(Homescreen(name="Homescreen"))
+screen_manager.add_widget(Lockscreen(name="Lockscreen"))
 
 class CustFloatLayout(FloatLayout):
     checkbox_is_active = ObjectProperty(False)
@@ -21,11 +32,14 @@ class CustFloatLayout(FloatLayout):
     def open_popup(self):
         the_popup = CustomPopup()
         the_popup.open()
+    
+    def spinner_clicked(self, value):
+        print("Spinner Value" + value)
 
 class Main_App(App):
     def build(self):
         Window.clearcolour = (1,1,1,1)
-        return CustFloatLayout()
+        return screen_manager
 
 Smart_Watch = Main_App()
 Smart_Watch.run()
