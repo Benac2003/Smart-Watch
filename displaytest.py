a4363cb57868cc0082b59f1b07317e813ea54e52
Benjamynn Weber <Benac2003@B23s-MacBook-Air.local>
2018-11-28 10:58:50 +0800

    update

--- displaytest.py
+++ displaytest.py
@@ -3,12 +3,12 @@ kivy.require("1.9.0")
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.pagelayout import PageLayout
-
+from kivy.properties import NumericProperty
 class GUI(PageLayout):
     pass
 
 class Main_App(App):
-    
+    hour = 10
     def build(self):
         return GUI()
 
