8adfe163b179f1564d6ff41e77829dfc94980a9f
Benjamynn Weber <Benac2003@B23s-Air.lan>
2018-11-28 18:50:42 +0800

    update

--- displaytest.py
+++ displaytest.py
@@ -1,16 +1,31 @@
 import kivy
-kivy.require("1.9.0")
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.pagelayout import PageLayout
-from kivy.properties import NumericProperty
-class GUI(PageLayout):
+from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
+from kivy.uix.popup import Popup
+from kivy.core.window import Window
+
+class CustomPopup(Popup):
     pass
 
+class CustFloatLayout(FloatLayout):
+    checkbox_is_active = ObjectProperty(False)
+
+    def switch_on(self, instance, value):
+        if value is True:
+            print("switch on")
+        else:
+            print("switch of")
+    
+    def open_popup(self):
+        the_popup = CustomPopup()
+        the_popup.open()
+
 class Main_App(App):
-    hour = 10
     def build(self):
-        return GUI()
+        Window.clearcolour = (1,1,1,1)
+        return CustFloatLayout()
 
 Smart_Watch = Main_App()
 Smart_Watch.run()
